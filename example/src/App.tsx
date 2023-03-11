30d630fbc8fe666e3269f0fb828f4d75a524eb00
Craig White <53788596+craigwh10@users.noreply.github.com>
2023-03-11 18:59:03 +0000

    fix: cleanup for rule timeoutand defaulted size (#6)
    * fix: cleanup for rule timeoutand defaulted size
    
    * chore(release): 1.5.0

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { AutomataGrid, conwaysGameOfLifePreset } from 'cellular-automata-react';
 
 function App() {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setState(1);
+    }, 2000)
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [])
+
+  if (state === 1) {
+    return <div className="App">
+    <AutomataGrid
+      rules={conwaysGameOfLifePreset} size={{
+        xWidth: 4,
+        yWidth: 4,
+      }}
+      pixelStyles={{
+        activeColor: 'purple',
+        inactiveColor: 'orange',
+        width: 10,
+        height: 10
+      }}
+      key={2}
+      className='a'
+      pixelsActive={[[1,1]]}
+      iterationTimeInMs={10000}
+    />
+  </div>
+  }
+
   return (
     <div className="App">
       <AutomataGrid
@@ -11,6 +44,7 @@ function App() {
           yWidth: 4
         }}
         className='a'
+        key={1}
         pixelsActive={[[1,1]]}
         iterationTimeInMs={1000}
       />
